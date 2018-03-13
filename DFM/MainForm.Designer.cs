65ea2b1e6651af2f4fd2088f4b0fa30fc1eddbaf
mtlsourcery <mtlsourcecode@gmail.com>
2018-03-13 14:27:57 -0500

    Open file functionality added, and small ui tweaks.

--- DFM/MainForm.Designer.cs
+++ DFM/MainForm.Designer.cs
@@ -30,21 +30,20 @@
         {
             this.fileLabel = new System.Windows.Forms.Label();
             this.typeLabel = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.filenameLabel = new System.Windows.Forms.Label();
+            this.FiletypeCBox = new System.Windows.Forms.ComboBox();
             this.fileopenButton = new System.Windows.Forms.Button();
+            this.FilenameTBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // fileLabel
             // 
             this.fileLabel.AutoSize = true;
             this.fileLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.fileLabel.Location = new System.Drawing.Point(158, 13);
+            this.fileLabel.Location = new System.Drawing.Point(170, 13);
             this.fileLabel.Name = "fileLabel";
             this.fileLabel.Size = new System.Drawing.Size(42, 16);
             this.fileLabel.TabIndex = 0;
             this.fileLabel.Text = "File: ";
-            this.fileLabel.Click += new System.EventHandler(this.label1_Click);
             // 
             // typeLabel
             // 
@@ -55,45 +54,45 @@
             this.typeLabel.Size = new System.Drawing.Size(115, 16);
             this.typeLabel.TabIndex = 1;
             this.typeLabel.Text = "Data File Type:";
-            this.typeLabel.Click += new System.EventHandler(this.typelabel_Click);
             // 
-            // comboBox1
+            // FiletypeCBox
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 32);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 2;
-            // 
-            // filenameLabel
-            // 
-            this.filenameLabel.AutoSize = true;
-            this.filenameLabel.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.filenameLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.filenameLabel.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.filenameLabel.Location = new System.Drawing.Point(152, 35);
-            this.filenameLabel.Name = "filenameLabel";
-            this.filenameLabel.Size = new System.Drawing.Size(112, 18);
-            this.filenameLabel.TabIndex = 3;
-            this.filenameLabel.Text = "No file selected...";
+            this.FiletypeCBox.FormattingEnabled = true;
+            this.FiletypeCBox.Location = new System.Drawing.Point(12, 32);
+            this.FiletypeCBox.Name = "FiletypeCBox";
+            this.FiletypeCBox.Size = new System.Drawing.Size(121, 24);
+            this.FiletypeCBox.TabIndex = 2;
             // 
             // fileopenButton
             // 
-            this.fileopenButton.Location = new System.Drawing.Point(287, 32);
+            this.fileopenButton.Location = new System.Drawing.Point(410, 34);
             this.fileopenButton.Name = "fileopenButton";
-            this.fileopenButton.Size = new System.Drawing.Size(92, 23);
+            this.fileopenButton.Size = new System.Drawing.Size(97, 24);
             this.fileopenButton.TabIndex = 4;
             this.fileopenButton.Text = "Select File";
             this.fileopenButton.UseVisualStyleBackColor = true;
+            this.fileopenButton.Click += new System.EventHandler(this.FileopenButton_Click);
+            // 
+            // FilenameTBox
+            // 
+            this.FilenameTBox.Enabled = false;
+            this.FilenameTBox.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.FilenameTBox.HideSelection = false;
+            this.FilenameTBox.Location = new System.Drawing.Point(173, 34);
+            this.FilenameTBox.MaxLength = 40;
+            this.FilenameTBox.Name = "FilenameTBox";
+            this.FilenameTBox.Size = new System.Drawing.Size(207, 22);
+            this.FilenameTBox.TabIndex = 5;
+            this.FilenameTBox.Text = "No file selected";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(404, 222);
+            this.ClientSize = new System.Drawing.Size(534, 285);
+            this.Controls.Add(this.FilenameTBox);
             this.Controls.Add(this.fileopenButton);
-            this.Controls.Add(this.filenameLabel);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.FiletypeCBox);
             this.Controls.Add(this.typeLabel);
             this.Controls.Add(this.fileLabel);
             this.Name = "MainForm";
@@ -108,9 +107,9 @@
 
         private System.Windows.Forms.Label fileLabel;
         private System.Windows.Forms.Label typeLabel;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label filenameLabel;
+        private System.Windows.Forms.ComboBox FiletypeCBox;
         private System.Windows.Forms.Button fileopenButton;
+        private System.Windows.Forms.TextBox FilenameTBox;
     }
 }
 
--- DFM/MainForm.cs
+++ DFM/MainForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -22,13 +23,39 @@ namespace DFM
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void FileopenButton_Click(object sender, EventArgs e)
         {
+            // Initialize a Stream to read in data from files
+            Stream myStream = null;
 
-        }
+            // Exactly what you think it is
+            OpenFileDialog openFileDialog = new OpenFileDialog();
 
-        private void typelabel_Click(object sender, EventArgs e)
-        {
+            // Set initial parameters for our OpenFileDialog
+            openFileDialog.InitialDirectory = "C:\\";
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|csv files (*.csv)|*.csv|xlsx" +
+                "files (*.xlsx)|.xlsx|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true; 
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            // Display the name of the selected file
+                            FilenameTBox.Text = openFileDialog.SafeFileName;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
 
         }
     }
