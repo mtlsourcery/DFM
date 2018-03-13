4d3bea63684376d1c7106ce2e5395609e3206ced
mtlsourcery <mtlsourcecode@gmail.com>
2018-03-13 17:06:52 -0500

    Added save folder open functionality and more UI views.

--- DFM/MainForm.Designer.cs
+++ DFM/MainForm.Designer.cs
@@ -28,76 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.fileLabel = new System.Windows.Forms.Label();
-            this.typeLabel = new System.Windows.Forms.Label();
+            this.FileLabel = new System.Windows.Forms.Label();
+            this.TypeLabel = new System.Windows.Forms.Label();
             this.FiletypeCBox = new System.Windows.Forms.ComboBox();
-            this.fileopenButton = new System.Windows.Forms.Button();
+            this.FileOpenButton = new System.Windows.Forms.Button();
             this.FilenameTBox = new System.Windows.Forms.TextBox();
+            this.SaveDirTBox = new System.Windows.Forms.TextBox();
+            this.SaveDirButton = new System.Windows.Forms.Button();
+            this.SaveDirLabel = new System.Windows.Forms.Label();
+            this.ProcessLabel = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.ProcessButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // fileLabel
+            // FileLabel
             // 
-            this.fileLabel.AutoSize = true;
-            this.fileLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.fileLabel.Location = new System.Drawing.Point(170, 13);
-            this.fileLabel.Name = "fileLabel";
-            this.fileLabel.Size = new System.Drawing.Size(42, 16);
-            this.fileLabel.TabIndex = 0;
-            this.fileLabel.Text = "File: ";
+            this.FileLabel.AutoSize = true;
+            this.FileLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FileLabel.Location = new System.Drawing.Point(176, 13);
+            this.FileLabel.Name = "FileLabel";
+            this.FileLabel.Size = new System.Drawing.Size(42, 16);
+            this.FileLabel.TabIndex = 0;
+            this.FileLabel.Text = "File: ";
             // 
-            // typeLabel
+            // TypeLabel
             // 
-            this.typeLabel.AutoSize = true;
-            this.typeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.typeLabel.Location = new System.Drawing.Point(12, 13);
-            this.typeLabel.Name = "typeLabel";
-            this.typeLabel.Size = new System.Drawing.Size(115, 16);
-            this.typeLabel.TabIndex = 1;
-            this.typeLabel.Text = "Data File Type:";
+            this.TypeLabel.AutoSize = true;
+            this.TypeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TypeLabel.Location = new System.Drawing.Point(12, 13);
+            this.TypeLabel.Name = "TypeLabel";
+            this.TypeLabel.Size = new System.Drawing.Size(115, 16);
+            this.TypeLabel.TabIndex = 1;
+            this.TypeLabel.Text = "Data File Type:";
             // 
             // FiletypeCBox
             // 
             this.FiletypeCBox.FormattingEnabled = true;
+            this.FiletypeCBox.Items.AddRange(new object[] {
+            "MicroPEM Data (.csv)",
+            "XRF Data (.rtf)",
+            "FWS Data (.xlsx)"});
             this.FiletypeCBox.Location = new System.Drawing.Point(12, 32);
             this.FiletypeCBox.Name = "FiletypeCBox";
-            this.FiletypeCBox.Size = new System.Drawing.Size(121, 24);
+            this.FiletypeCBox.Size = new System.Drawing.Size(155, 24);
             this.FiletypeCBox.TabIndex = 2;
             // 
-            // fileopenButton
+            // FileOpenButton
             // 
-            this.fileopenButton.Location = new System.Drawing.Point(410, 34);
-            this.fileopenButton.Name = "fileopenButton";
-            this.fileopenButton.Size = new System.Drawing.Size(97, 24);
-            this.fileopenButton.TabIndex = 4;
-            this.fileopenButton.Text = "Select File";
-            this.fileopenButton.UseVisualStyleBackColor = true;
-            this.fileopenButton.Click += new System.EventHandler(this.FileopenButton_Click);
+            this.FileOpenButton.Location = new System.Drawing.Point(443, 34);
+            this.FileOpenButton.Name = "FileOpenButton";
+            this.FileOpenButton.Size = new System.Drawing.Size(97, 24);
+            this.FileOpenButton.TabIndex = 4;
+            this.FileOpenButton.Text = "Select File";
+            this.FileOpenButton.UseVisualStyleBackColor = true;
+            this.FileOpenButton.Click += new System.EventHandler(this.FileOpenButton_Click);
             // 
             // FilenameTBox
             // 
-            this.FilenameTBox.Enabled = false;
             this.FilenameTBox.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.FilenameTBox.HideSelection = false;
-            this.FilenameTBox.Location = new System.Drawing.Point(173, 34);
+            this.FilenameTBox.Location = new System.Drawing.Point(179, 34);
             this.FilenameTBox.MaxLength = 40;
             this.FilenameTBox.Name = "FilenameTBox";
-            this.FilenameTBox.Size = new System.Drawing.Size(207, 22);
+            this.FilenameTBox.Size = new System.Drawing.Size(249, 22);
             this.FilenameTBox.TabIndex = 5;
             this.FilenameTBox.Text = "No file selected";
+            // 
+            // SaveDirTBox
+            // 
+            this.SaveDirTBox.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.SaveDirTBox.HideSelection = false;
+            this.SaveDirTBox.Location = new System.Drawing.Point(179, 91);
+            this.SaveDirTBox.MaxLength = 40;
+            this.SaveDirTBox.Name = "SaveDirTBox";
+            this.SaveDirTBox.Size = new System.Drawing.Size(249, 22);
+            this.SaveDirTBox.TabIndex = 6;
+            this.SaveDirTBox.Text = "No folder selected";
+            // 
+            // SaveDirButton
+            // 
+            this.SaveDirButton.Location = new System.Drawing.Point(443, 91);
+            this.SaveDirButton.Name = "SaveDirButton";
+            this.SaveDirButton.Size = new System.Drawing.Size(97, 24);
+            this.SaveDirButton.TabIndex = 7;
+            this.SaveDirButton.Text = "Select Folder";
+            this.SaveDirButton.UseVisualStyleBackColor = true;
+            this.SaveDirButton.Click += new System.EventHandler(this.SaveDirButton_Click);
+            // 
+            // SaveDirLabel
+            // 
+            this.SaveDirLabel.AutoSize = true;
+            this.SaveDirLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaveDirLabel.Location = new System.Drawing.Point(176, 72);
+            this.SaveDirLabel.Name = "SaveDirLabel";
+            this.SaveDirLabel.Size = new System.Drawing.Size(119, 16);
+            this.SaveDirLabel.TabIndex = 8;
+            this.SaveDirLabel.Text = "Save Directory: ";
+            // 
+            // ProcessLabel
+            // 
+            this.ProcessLabel.AutoSize = true;
+            this.ProcessLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProcessLabel.Location = new System.Drawing.Point(9, 135);
+            this.ProcessLabel.Name = "ProcessLabel";
+            this.ProcessLabel.Size = new System.Drawing.Size(147, 16);
+            this.ProcessLabel.TabIndex = 9;
+            this.ProcessLabel.Text = "Processing Options:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 165);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(179, 20);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Fill lab analysis certificate";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 191);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(156, 20);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.Text = "Export neph. data plot";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(12, 217);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(237, 20);
+            this.checkBox3.TabIndex = 12;
+            this.checkBox3.Text = "Export combined data spreadsheet";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // ProcessButton
+            // 
+            this.ProcessButton.Location = new System.Drawing.Point(363, 217);
+            this.ProcessButton.Name = "ProcessButton";
+            this.ProcessButton.Size = new System.Drawing.Size(177, 24);
+            this.ProcessButton.TabIndex = 13;
+            this.ProcessButton.Text = "Process Output";
+            this.ProcessButton.UseVisualStyleBackColor = true;
+            this.ProcessButton.Click += new System.EventHandler(this.ProcessButton_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(534, 285);
+            this.ClientSize = new System.Drawing.Size(552, 260);
+            this.Controls.Add(this.ProcessButton);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.ProcessLabel);
+            this.Controls.Add(this.SaveDirLabel);
+            this.Controls.Add(this.SaveDirButton);
+            this.Controls.Add(this.SaveDirTBox);
             this.Controls.Add(this.FilenameTBox);
-            this.Controls.Add(this.fileopenButton);
+            this.Controls.Add(this.FileOpenButton);
             this.Controls.Add(this.FiletypeCBox);
-            this.Controls.Add(this.typeLabel);
-            this.Controls.Add(this.fileLabel);
+            this.Controls.Add(this.TypeLabel);
+            this.Controls.Add(this.FileLabel);
             this.Name = "MainForm";
             this.Text = "MTL Data File Manager";
-            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,11 +204,19 @@
 
         #endregion
 
-        private System.Windows.Forms.Label fileLabel;
-        private System.Windows.Forms.Label typeLabel;
+        private System.Windows.Forms.Label FileLabel;
+        private System.Windows.Forms.Label TypeLabel;
         private System.Windows.Forms.ComboBox FiletypeCBox;
-        private System.Windows.Forms.Button fileopenButton;
+        private System.Windows.Forms.Button FileOpenButton;
         private System.Windows.Forms.TextBox FilenameTBox;
+        private System.Windows.Forms.TextBox SaveDirTBox;
+        private System.Windows.Forms.Button SaveDirButton;
+        private System.Windows.Forms.Label SaveDirLabel;
+        private System.Windows.Forms.Label ProcessLabel;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button ProcessButton;
     }
 }
 
--- DFM/MainForm.cs
+++ DFM/MainForm.cs
@@ -11,37 +11,48 @@ using System.Windows.Forms;
 
 namespace DFM
 {
+    /// <summary>
+    /// The main application Window 
+    /// </summary>
     public partial class MainForm : Form
     {
+        // Class variables
+        
+
+        /// <summary>
+        /// The constructor for MainForm.
+        /// </summary>
         public MainForm()
         {
             InitializeComponent();
         }
 
-        private void MainForm_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void FileopenButton_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Handles FileOpenButton click event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FileOpenButton_Click(object sender, EventArgs e)
         {
             // Initialize a Stream to read in data from files
             Stream myStream = null;
 
-            // Exactly what you think it is
+            // Displays the file selection window and stores the result
             OpenFileDialog openFileDialog = new OpenFileDialog();
 
             // Set initial parameters for our OpenFileDialog
             openFileDialog.InitialDirectory = "C:\\";
-            openFileDialog.Filter = "txt files (*.txt)|*.txt|csv files (*.csv)|*.csv|xlsx" +
-                "files (*.xlsx)|.xlsx|All files (*.*)|*.*";
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|csv files " +
+                "(*.csv)|*.csv|xlsx files (*.xlsx)|.xlsx|All files (*.*)|*.*";
             openFileDialog.FilterIndex = 1;
             openFileDialog.RestoreDirectory = true; 
 
+            // Try to open the file selection dialog
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 try
                 {
+                    // Check if the selected is null
                     if ((myStream = openFileDialog.OpenFile()) != null)
                     {
                         using (myStream)
@@ -53,10 +64,62 @@ namespace DFM
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                    MessageBox.Show("Error: Could not read file from disk. " +
+                        "Original error: " + ex.Message);
                 }
             }
+        }
 
+        /// <summary>
+        /// Handles SaveDirButton click event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SaveDirButton_Click(object sender, EventArgs e)
+        {
+            // Displays directory selection window and stores the result
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            // Set the help text description for folderBrowserDialog
+            folderBrowserDialog.Description = "Select the save directory for " +
+                "the output file(s).";
+
+            // Set default directory
+            //folderBrowserDialog.RootFolder = Environment.SpecialFolder.MyDocuments;
+
+            // Try to open the folder selection dialog
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if (folderBrowserDialog.SelectedPath != null)
+                    {
+                        // Display the name of the selected directory
+                        SaveDirTBox.Text = folderBrowserDialog.SelectedPath;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. " +
+                        "Original error: " + ex.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handles ProcessButton click event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ProcessButton_Click(object sender, EventArgs e)
+        {
+            // Do the processing of selected file (extracting csv rows, cols,
+            // and putting into a "nicer" file; populating a lab analysis 
+            // certificate; plotting stuff; etc. then save the output to 
+            // the specified directory. 
+
+            //TODO: put check boxes in MainForm to select what processing tasks
+            // to perform
         }
     }
 }
